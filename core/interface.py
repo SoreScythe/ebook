e43ac845acc2e2ac67e25a5c8112b57081aead4f
SoreScythe <jbauso937@gmail.com>
2021-06-05 10:41:29 +0800

    working interface module to parse arguments

---
+++ core/interface.py
@@ -0,0 +1,11 @@
+## module to parse the arguments
+from sys import argv
+import argparse
+
+parser = argparse.ArgumentParser(
+                        prog="Ebook Author",
+                        description="A simp",
+                        epilog="please use wisely for the author is not so smart"
+)
+parser.add_argument('-o', '--output', required=True, help="path to the output file", type=str)
+args = parser.parse_args(argv[1:])
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+prog = "Ebook Author"
+descr = "A module to create clean and light epubs."
+
+if __name__ == "__main__":
+    from core import interface
+    print(interface.args)
